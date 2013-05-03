7a9283beaa6cc04153613efde5cdaadaf3eb7065
Swaroop SM <swaroop.striker@gmail.com>
2013-05-03 19:13:30 +0530

    initialized the app

---
+++ README.md
@@ -0,0 +1,3 @@
+## R-Drive 
+
+R-Drive is a campus recruitment/placements/hiring application that bridges the gap between companies and/or colleges.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RDrive::Application.config.secret_token = 'b1c679c05e915ab4307ffe8350a87baa5fd7c5731047ef5d66178a83a99e0d782e7ef01f28d96066ae0286950ac4b1dc56d0baa8f040d0d5a5d6c13c01d53259'
