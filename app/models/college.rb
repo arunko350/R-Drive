718ebeceda2aa3b5b4d489010bdc85c626e8c284
Swaroop SM <swaroop.striker@gmail.com>
2013-05-06 17:38:09 +0530

    added some custom validations to college model

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,7 +1,28 @@
 class College < ActiveRecord::Base
   attr_accessible :description, :name, :website, :university, :code
 
+  serialize :contact, ActiveRecord::Coders::Hstore
+  serialize :address, ActiveRecord::Coders::Hstore
+
   validates_presence_of :description, :name, :website, :university, :code
   validates_uniqueness_of :name, :website, :code
+  validate :contact_address_validation
+  validate :contact_uniqueness_validation
+
+  private
+  def contact_address_validation 
+  	errors.add(:base, "Email can't be empty") if self.contact[:email].blank?
+  	errors.add(:base, "Atleast provide one phone number") if self.contact[:office].blank? && self.contact[:mobile].blank?
+  	errors.add(:base, "City can't be blank") if self.address[:city].blank?
+  	errors.add(:base, "State can't be blank") if self.address[:state].blank?
+  	errors.add(:base, "Country can't be blank") if self.address[:country].blank?
+  	errors.add(:base, "Atleast provide one line address") if self.address[:line1].blank? && self.address[:line2].blank?
+  	# @TODO: Pincode validation so that it takes only digits.
+  	errors.add(:base, "Pincode must have exactly 6 digits") if self.address[:pincode].delete(" ").length != 6
+  end
+
+  def contact_uniqueness_validation
+  	errors.add(:base, "Contact Email appears to be of someother college's") if College.where("contact -> 'email' = :contact_email", :contact_email => self.contact[:email].strip).exists?
+  end
 
 end
