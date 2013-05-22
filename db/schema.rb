378b0207ec28e302fb487c94fd95e85d18b66a0f
Swaroop SM <swaroop.striker@gmail.com>
2013-05-22 09:27:48 +0530

    generated course model and ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130520142116) do
+ActiveRecord::Schema.define(:version => 20130522035711) do
 
   create_table "colleges", :force => true do |t|
     t.string   "name"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(:version => 20130520142116) do
     t.string   "university"
     t.string   "code"
     t.string   "website"
+  end
+
+  create_table "courses", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "roles", :force => true do |t|
